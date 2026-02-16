0ab5eb50d157d245f3f420fbe263b286a37e5b89
isayantan <sayantan.choudhury4@gmail.com>
2026-02-16 11:26:23 +0400

    fixed initialization as per GPT2 paper

--- train_gpt2.py
+++ train_gpt2.py
@@ -14,6 +14,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output ptojection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         #regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -45,6 +46,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate = 'tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -92,6 +94,22 @@ class GPT(nn.Module):
 
         # weight sharing scheme
         self.transformer.wte.weight = self.lm_head.weight
+
+        # init params
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            # check "Language Models are Unsupervised Multitask Learners" paper for more details on the 0.02 value and scaling factor
+            # for the linear layers in the attention and MLP, we use a scaling 1/\sqrt{number of layers} for initialization to improve stability
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean = 0.0, std = std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean = 0.0, std = 0.02)
 
     def forward(self, idx, targets = None):
         # idx is of shape (B, T)
@@ -164,6 +182,7 @@ class GPT(nn.Module):
         return model 
 
 # -----------------------------------------------------------------
+# data loading
 import tiktoken
 file = "data/tiny_shakespeare.txt"
 
@@ -195,8 +214,15 @@ class DataLoaderLite:
             self.current_position = 0
         return x, y
 
-# ------------------------------------------------------------------
+# -----------------------------------------------------------------
+# set device to cuda:2
 device = "cuda:2"
+
+# set seed for reproducibility
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(1337)
+
 train_loader = DataLoaderLite(B = 4, T = 32)
 
 # get logits
